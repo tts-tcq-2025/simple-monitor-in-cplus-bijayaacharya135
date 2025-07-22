b4d2e504882fb22f13d74323d2044b6f478b2288
bijayaacharya135 <internbijaya@gmail.com>
2025-07-22 12:44:06 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 
+// Pure functions for individual checks
 bool isTemperatureOk(float temperature) {
   return temperature >= 0 && temperature <= 45;
 }
@@ -13,10 +14,13 @@ bool isSocOk(float soc) {
 bool isChargeRateOk(float chargeRate) {
   return chargeRate <= 0.8;
 }
+
+// Pure function: no side effects, returns combined result with bitwise &
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
+  return isTemperatureOk(temperature) & isSocOk(soc) & isChargeRateOk(chargeRate);
 }
 
+// Separate function for printing reasons
 void checkBatteryAndPrint(float temperature, float soc, float chargeRate) {
   if (!isTemperatureOk(temperature)) {
     cout << "Temperature out of range!\n";
@@ -28,7 +32,6 @@ void checkBatteryAndPrint(float temperature, float soc, float chargeRate) {
     cout << "Charge Rate out of range!\n";
   }
 }
-
 int main() {
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
